a8720260ea87a6bb2033039314258e5c1ced212d
Bombonzinha <medifisq@gmail.com>
2025-03-24 01:20:08 -0300

    Fixed inconsistencies and started to work on Members. Plans:Configure each member flag(using ns flag url) + logo (locally)

--- src/content/tournaments.ts
+++ src/content/tournaments.ts
@@ -1,32 +1,38 @@
 export const tournaments = [
   {
     "id": 1,
-    "name": "Forest Cup Bilsa 2020",
-    "image": "/src/assets/images/forest-cup/forest-cup-2024.png",
+    "name": "Bilsa 2020",
+    "image": "/src/assets/images/forest-cup/forest-cup-2020.png",
     "year": 2020,
     "winner": "Ruinenlust",
     "runner_up": "Cannibaland",
-    "top_scorer": "Nathan Relizorus",
-    "route": "/forest-cup/forest-cup-2024"
+    "route": "/forest-cup/forest-cup-2020"
   },
   {
     "id": 2,
-    "name": "Forest Cup Bilsa 2022",
-    "image": "/src/assets/images/forest-cup/forest-cup-2024.png",
+    "name": "Bilsa 2022",
+    "image": "/src/assets/images/forest-cup/forest-cup-2022.png",
     "year": 2022,
     "winner": "Atsvea",
     "runner_up": "Cacusia",
-    "top_scorer": "Nathan Relizorus",
-    "route": "/forest-cup/forest-cup-2024"
+    "route": "/forest-cup/forest-cup-2022"
   },
   {
     "id": 3,
-    "name": "Forest Cup Anxious & Kevin 2024",
+    "name": "Anxious & Kevin 2024",
     "image": "/src/assets/images/forest-cup/forest-cup-2024.png",
     "year": 2024,
     "winner": "Nardin",
     "runner_up": "Jutsa",
-    "top_scorer": "Nathan Relizorus",
     "route": "/forest-cup/forest-cup-2024"
-  }
+  },
+  {
+    "id": 4,
+    "name": "Forest Cup 2026",
+    "image": "/src/assets/images/forest-cup/forest-cup-2026.png",
+    "year": 2026,
+    "winner": "-",
+    "runner_up": "-",
+    "route": "/forest-cup/forest-cup-2026"
+  },
 ];
