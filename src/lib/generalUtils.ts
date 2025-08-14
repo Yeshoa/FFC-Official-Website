f94b9f7c07e078c37b9954002de8e692ddf0c203
Bombonzinha <medifisq@gmail.com>
2025-08-13 22:01:21 -0300

    feat(LOCALE): Add new constant for Locale, set as 'en-US'

--- src/lib/generalUtils.ts
+++ src/lib/generalUtils.ts
@@ -1,10 +1,14 @@
 import { getCollection } from 'astro:content';
 
+// CONSTANTS
+// Form links
 export const NEW_MEMBERS_FORM_LINK = 'https://docs.google.com/forms/d/e/1FAIpQLScR44EQbFVoiMEaVJKN8tOy2JiNISN_OUTtorWkOUR6pAc6Xg/viewform?usp=dialog';
 export const FFC_MEMBERS_FORM_LINK = 'https://docs.google.com/forms/d/e/1FAIpQLSeRMcQ7UNr0wXewbL6OMqv60DMpvHTuA8XRclmYljGunNX-Ng/viewform?usp=dialog';
-export const ENABLE_DONATIONS = false;
 export const SPONSOR_FORM_LINK = 'https://docs.google.com/forms/d/e/1FAIpQLSfKC5ptPjarwqpH0_KkGyhXrgB2ekWj6fHyMbhGyqf0pjDzsA/viewform?usp=dialog';
 
+export const ENABLE_DONATIONS = false;
+export const LOCALE = 'en-US';
+// CACHES
 let membersCache: any[] | null = null;
 let tournamentsCache: any[] | null = null;
 let matchesCache: any[] | null = null;
@@ -12,6 +16,7 @@ let achievementsCache: any[] | null = null;
 let articlesCache: any[] | null = null;
 let sponsorsCache: any[] | null = null;
 
+// GETTERS
 export async function getMembers() {
   if (!membersCache) membersCache = await getCollection('members');
   return membersCache;
