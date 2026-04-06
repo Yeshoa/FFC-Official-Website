08adbfc9df9546ae65cfce1138a79c631a0154ad
Bombonzinha <medifisq@gmail.com>
2026-04-06 20:44:57 -0300

    feat(New Article): Roless

--- src/utils/articleUtils.ts
+++ src/utils/articleUtils.ts
@@ -7,3 +7,7 @@ export function getArticlesByTags(articles: any[], tags: string[] = []): any[] {
       );
   return filtered.sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
 }
+
+// This is for external hosted images
+export const isExternalUrl = (src: unknown): src is string =>
+  typeof src === 'string' && src.startsWith('http');
